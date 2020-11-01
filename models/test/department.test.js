ef12b3ab6bf3cf3c86c982472c21795036f4cf21
Magdalena Świderska <m.swiderska@outlook.com>
2020-11-01 03:22:31 +0100

    Add the rest of the tests for department

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -13,6 +13,48 @@ describe('Department', () => {
         });
     });
 
+    it('should throw an error if "name" is not a string', () => {
+
+        const cases = [{}, []];
+        for(let name of cases) {
+          const dep = new Department({ name });
+      
+          dep.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+      
+        }
+      
+    });
+
+    /*it('should throw an error if "name" is too short or too long', () => {
+
+        const cases = ['abcd', 'Lorem Ipsum, Lorem Ip']; // we test various cases, some of them are too short, some of them are too long
+        for(let name of cases) {
+          const dep = new Department({ name });
+      
+          dep.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+      
+        }
+      
+    });*/
+
+    it('should not throw an error if "name" is okay', () => {
+
+        const cases = ['Management', 'Human Resources'];
+        for(let name of cases) {
+          const dep = new Department({ name });
+      
+          dep.validate(err => {
+            expect(err).to.not.exist;
+          });
+      
+        }
+      
+      });
+
     after(() => {
         mongoose.models = {};
     });
