c9450d1122738e9d3048cfaf86eb35afa281282b
Magdalena Świderska <m.swiderska@outlook.com>
2020-11-01 03:07:13 +0100

    Add test to department & tap npm & node modules unable

---
+++ models/test/department.test.js
@@ -0,0 +1,22 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+
+    it('should throw an error if no "name" arg', () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+
+        dep.validate(err => {
+            expect(err.errors.name).to.exist;
+        });
+    });
+
+    after(() => {
+        mongoose.models = {};
+    });
+  
+  });
+
+  
