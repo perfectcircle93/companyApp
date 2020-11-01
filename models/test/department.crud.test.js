281f98d45180f7fba36e806525c1f6104b1221b1
Magdalena Świderska <m.swiderska@outlook.com>
2020-11-01 04:13:13 +0100

    Check Read method in department

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -19,4 +19,33 @@ describe('Department', () => {
       
       });
 
+    describe('Reading data', () => {
+
+        before(async () => {
+          const testDepOne = new Department({ name: 'Department #1' });
+          await testDepOne.save();
+      
+          const testDepTwo = new Department({ name: 'Department #2' });
+          await testDepTwo.save();
+        });
+      
+
+        it('should return all the data with "find" method', async () => {
+            const departments = await Department.find();
+            const expectedLength = 2;
+            expect(departments.length).to.be.equal(expectedLength);
+        });
+
+        it('should return a proper document by "name" with "findOne" method', async () => {
+            const department = await Department.findOne({ name: 'Department #1' });
+            const expectedName = 'Department #1';
+            expect(department.name).to.be.equal('Department #1');
+        });
+
+        after(async () => {
+            await Department.deleteMany();
+          });
+      
+    });  
+
 });
